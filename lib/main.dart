aaca2b12d46d978880b0fecfaead77b10f8fa817
pauek <pau.fernandez@upc.edu>
2019-11-15 16:24:50 +0100

    Canvio títol i canvis a pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Firebase Demo',
       home: Scaffold(),
     );
   }
